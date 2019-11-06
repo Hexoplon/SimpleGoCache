e606e5ff47eafc617e73581f2940452d2226e958
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2019-11-06 22:54:45 +0100

    Added const file for const strings, like error messages

---
+++ consts.go
@@ -0,0 +1,18 @@
+package simplegocache
+
+// Errors
+
+// ErrorAlreadyExists - Entry already exists for given key
+const ErrorAlreadyExists = "An entry already exists in cache for this key"
+
+// ErrorTooLarge - Content is larger than specified max size
+const ErrorTooLarge = "Content is larger than specified max size"
+
+// ErrorTooManyEntries - Too many entries in cache
+const ErrorTooManyEntries = "Too many entries in cache"
+
+// ErrorNoEntry - No entry found for given key
+const ErrorNoEntry = "No entry found for given key"
+
+// ErrorCanNotDelete - Unable to delete entry
+const ErrorCanNotDelete = "Unable to delete entry"
